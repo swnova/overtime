81aab2833d229cc4f03949c50227cc5e63ab5a09
swnova <sharrodwillanova@gmail.com>
2022-10-06 02:22:43 -0700

    working js file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,41 @@
 //Date + Time display//
 var todaysDate = 0
+var relTime = moment().hours;
 var runningDate = setInterval(function() {
     todaysDate = moment().format('MMMM Do YYYY');
     $("#currentDay").html(todaysDate);
 
 var timer = moment().format('h:mm:ss');
     $('#timer').html(timer);
-}, 100) // 
-var currentTime = moment().hour;
-console.log(timer)
+}, 100)
+
+//save button listener
+var saveBtn = $(".saveBtn")
+
+saveBtn.on("click", function() {
+    var text = $(this).siblings("text").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text)
+})
+
+//console.log(saveBtn.on)
+console.log("timer")
+function dataTiming() {
+    
+
+    $("#block").each(function() {
+        var currentBlock = parseInt($(this).attr("id"));
+
+        if (currentblock > relTime ) {
+            $(this).addClass("future");
+        }       else if (currentBlock === timenow) {
+            $(this).addClass("present");
+        }       else {
+            $(this).addClass("past");
+        }
+    })
+    
+};
+
 
 
