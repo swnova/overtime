39b893aead283d4a4951019cefe756bcb80692d7
swnova <sharrodwillanova@gmail.com>
2022-10-06 16:45:53 -0700

    adding updates to HTML span time numeration

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,34 @@
 //Date + Time display//
-var todaysDate = 0
+var todaysDate = 0;
 var relTime = moment().hours;
 var runningDate = setInterval(function() {
     todaysDate = moment().format('MMMM Do YYYY');
     $("#currentDay").html(todaysDate);
-
+//var text = jqueryval(text);
+var time = moment().hours;
+//var currentBlock = val(span.val)
 var timer = moment().format('h:mm:ss');
     $('#timer').html(timer);
 }, 100)
 
 //save button listener
-var saveBtn = $(".saveBtn")
+var saveBtn = $(".saveBtn");
 
 saveBtn.on("click", function() {
     var text = $(this).siblings("text").val();
+    console.log(text)
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text)
-})
+});
 
 //console.log(saveBtn.on)
-console.log("timer")
-function dataTiming() {
-    
 
-    $("#block").each(function() {
-        var currentBlock = parseInt($(this).attr("id"));
+//function dataTiming() {
+    //console.log(hello)
 
+    $(".input-group").forEach(function() {
+        var currentBlock = $("span-class").val();
+      console.log(currentBlock)  
         if (currentblock > relTime ) {
             $(this).addClass("future");
         }       else if (currentBlock === timenow) {
@@ -35,7 +38,7 @@ function dataTiming() {
         }
     })
     
-};
+//};
 
-
+//dataTiming()
 
